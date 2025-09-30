8179bd9b020c417f92169c199c29c81c6f1392ac
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2025-09-30 12:10:01 +0300

    Connect MySQL database

--- Shop.Server/Program.cs
+++ Shop.Server/Program.cs
@@ -1,3 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shop.DATA;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+//Get connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySql(connectionString, serverVersion));
+
 
 var app = builder.Build();
+
+// Try connect to database
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    try
+    {
+        await context.Database.CanConnectAsync();
+        Console.WriteLine("✅ Database connection successful!");
+
+        // Apply migrations automatically
+        await context.Database.MigrateAsync();
+        Console.WriteLine("✅ Database migrations applied!");
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"❌ Database setup failed: {ex.Message}");
+    }
+}
+
+app.MapHealthChecks("/health");
+
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
---
+++ shop.DATA/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace shop.DATA
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+    }
+}
